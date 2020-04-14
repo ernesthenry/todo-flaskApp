44827ed6ae638675aa4d384277ad05750ec69252
ernesthenry <henry38ernest@gmail.com>
2020-04-15 01:31:47 +0300

    Get user data in flask

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db=SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
+db=SQLAlchemy(app)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
@@ -15,6 +15,15 @@ class Todo(db.Model):
 
 db.create_all()
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
+
